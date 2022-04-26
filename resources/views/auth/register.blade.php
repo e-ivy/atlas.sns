5557c67f78e2b1d43d6ba5e996640a802c916d74
MSI\ss <english.ive.ss@gmail.com>
2022-04-26 11:38:06 +0900

    snsデータ追加

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.logout')
+
+@section('content')
+
+{!! Form::open(['url' => '/register']) !!}
+
+<h2>新規ユーザー登録</h2>
+
+<!-- 編集前
+
+{{ Form::label('ユーザー名') }}
+{{ Form::text('username',null,['class' => 'input']) }}
+
+{{ Form::label('メールアドレス') }}
+{{ Form::text('mail',null,['class' => 'input']) }}
+
+{{ Form::label('パスワード') }}
+{{ Form::text('password',null,['class' => 'input']) }}
+
+{{ Form::label('パスワード確認') }}
+{{ Form::text('password-confirm',null,['class' => 'input']) }}
+
+{{ Form::submit('登録') }} -->
+
+{{ Form::label('ユーザー名') }}
+{{ Form::text('username',null,['class' => 'input']) }}
+
+{{ Form::label('メールアドレス') }}
+{{ Form::text('mail',null,['class' => 'input']) }}
+
+{{ Form::label('パスワード') }}
+{{ Form::text('password',null,['class' => 'input']) }}
+
+{{ Form::label('パスワード確認') }}
+{{ Form::text('password-confirm',null,['class' => 'input']) }}
+
+{{ Form::submit('登録',['class'=>'btn btn-primary']) }}
+
+
+<p><a href="/login">ログイン画面へ戻る</a></p>
+
+{!! Form::close() !!}
+
+
+@endsection
